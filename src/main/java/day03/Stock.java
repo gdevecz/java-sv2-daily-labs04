922e7076877f02735e3a7b29d784c93773df436f
gdevecz <gdevecz@gmail.com>
2021-11-05 02:08:09 +0100

    Day03 feladat Azt hiszem kicsit félreértettem a feladatot :)

--- src/main/java/day03/Stock.java
+++ src/main/java/day03/Stock.java
@@ -12,27 +12,36 @@ public class Stock {
     public double maxProfit() {
         double maxSum = 0.0;
         double localSum = 0.0;
-
+        double min = shares.get(0);
+        double max = shares.get(0);
         int i = 1;
         while (i < shares.size()) {
-            while (i < shares.size()
-                    && !(shares.get(i) - shares.get(i - 1) > 0.0)) {
-                i++;
+            if(shares.get(i) > max) {
+                max = shares.get(i);
             }
-            localSum = shares.get(i - 1);
-            while (i < shares.size()
-                    && !(shares.get(i) - shares.get(i - 1) < 0.0)) {
-                i++;
+            if(shares.get(i) < min ) {
+                min = shares.get(i);
             }
-            localSum = shares.get(i - 1) - localSum;
-//             Igazából ez lenne a maximális haszon
+            // Ez a többihez kéne, ezért nem értettem a feladatot
+//            while (i < shares.size()
+//                    && !(shares.get(i) - shares.get(i - 1) > 0.0)) {
+//                i++;
+//            }
+//            localSum = shares.get(i - 1);
+//            while (i < shares.size()
+//                    && !(shares.get(i) - shares.get(i - 1) < 0.0)) {
+//                i++;
+//            }
+//            localSum = shares.get(i - 1) - localSum;
+//             Igazából ez lenne a maximális haszon( a felszálló ágakban bent van, a leszálló ágak idején másik részvényben)
 //            maxSum += sum;
-//            Ez csak egyszeri vétel eladási maximum
-            if (localSum > maxSum) {
-                maxSum = localSum;
-            }
+//            Ez csak egyszeri max lokális emelkedésből származó haszon
+//            if (localSum > maxSum) {
+//                maxSum = localSum;
+//            }
             i++;
         }
+        maxSum = max-min;
         return maxSum;
     }
 }
